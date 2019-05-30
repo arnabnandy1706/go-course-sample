1f42b640df9790c58a40304df203ea09e303c8d7
Arnab Kumar Nandy <arnab.nandy1991@gmail.com>
2019-05-30 18:21:08 +0530

    Added New modules

--- 14_web/main.go
+++ 14_web/main.go
@@ -9,8 +9,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Arnab !!! Congrats !!! This is your first GO WEB Program !!! ")
 }
 
+func indexTwo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Arnab !!! Congrats !!! You have reached Page 2 now !!! ")
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/page2", indexTwo)
 	fmt.Println("Server Starting.....")
 	http.ListenAndServe(":3000", nil)
 	fmt.Println("Server Started !!!")
